287b39fb4bca7e3f590594efd6f34fb2a8ca0d0b
DevalapalleJaswanth <jaswanthreddy040@gmail.com>
2022-03-30 04:41:35 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-grvdmy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-grvdmy)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+import LoginPage from './Login';
+import SignUp from './SignUp';
+
+export default function App() {
+  return (
+    <div className="App">
+      {/* <LoginPage /> */}
+      <SignUp />
+    </div>
+  );
+}
