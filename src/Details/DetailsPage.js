e31f7766954e9a7a56b514a44240b358c80149e0
DevalapalleJaswanth <jaswanthreddy040@gmail.com>
2022-03-31 00:11:47 +0530

    2 files modified

--- src/Details/DetailsPage.js
+++ src/Details/DetailsPage.js
@@ -3,54 +3,44 @@ import axios from 'axios';
 import moment from 'moment';
 import './DetailsPage.css';
 import { Banner, Chart } from '../Components';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
 export default function DetailsPage() {
   const [data, setData] = useState(null);
   const [mfData, setMFData] = useState();
-  useEffect(() => {
-    const fetchfunc = async () => {
-      let resp = await axios.get('https://api.mfapi.in/mf/100350');
-      let data = await resp.data;
-
-      console.log(data.data);
-      if (data && data.data.length > 0) {
-        let temp = data.data.map((ele, i) => {
-          let pair = Object.values(ele);
-          pair[0] = Number(moment(pair[0], 'DD-MM-YYYY').format('x'));
-          pair[1] = Number(pair[1]);
-          //console.log(pair, date);
-          return pair;
-        });
-        setData([...temp].reverse());
-        console.log(temp);
-        setMFData({ ...data.meta });
-      }
-    };
-
-    fetchfunc();
-    // console.log(data);
-  }, []);
+  const dispatch = useDispatch();
+  const mutualFund = useSelector((state) => state.mutualFund);
+  const { id } = useParams();
+  let mfDetails =
+    mutualFund && id
+      ? mutualFund.filter((ele, i) => {
+          if (ele.meta.scheme_code === id) {
+            return ele;
+          }
+        })
+      : '';
 
   return (
     <div className="details-container">
       <Banner text="Mutual Fund Details" />
       <div className="container">
         <div className="sub-container title">scheme name</div>
-        <div>{mfData && mfData.scheme_name}</div>
+        <div>{mfDetails.length > 0 && mfDetails[0].meta.scheme_name}</div>
       </div>
       <div className="container">
         <div className="sub-container title">scheme type</div>
-        <div>{mfData && mfData.scheme_type}</div>
+        <div>{mfDetails.length > 0 && mfDetails[0].meta.scheme_type}</div>
       </div>
       <div className="container">
         <div className="sub-container title">scheme code</div>
-        <div>{mfData && mfData.scheme_code}</div>
+        <div>{mfDetails.length > 0 && mfDetails[0].meta.scheme_code}</div>
       </div>
       <div className="container">
         <div className="sub-container title">fund house</div>
-        <div>{mfData && mfData.fund_house}</div>
+        <div>{mfDetails.length > 0 && mfDetails[0].meta.fund_house}</div>
       </div>
       <div className="container">
-        <Chart data={data} />
+        <Chart data={mfDetails.length > 0 && mfDetails[0].data} />
       </div>
     </div>
   );
